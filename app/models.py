f6db4cba49342a4403dd5829d2ca476866ca0e5e
AashayBhupendraDoshi <aashaydoshi97@gmail.com>
2022-09-23 16:32:08 +0530

    remove tkinter bug

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from enum import unique
-from tkinter import CASCADE
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import TIMESTAMP
